e45296f40a1dd4b7232cdccc4e13a68d6a158398
ThomasSzentirmay <132217664+ThomasSzentirmay@users.noreply.github.com>
2023-07-25 21:16:09 -0400

    wrote get route for getting user info from id

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,16 +1,31 @@
-const express = require('express');
-const { User } = require('../models');
+const express = require("express");
+const { User } = require("../models");
 
 const router = express.Router();
 
 // get all the users
-router.get('/', async (req, res) => {
-    try {
-      const users = await User.find().populate('thoughts').populate('friends');
-      res.json(users);
-    } catch (err) {
-      res.status(500).json(err);
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find().populate("thoughts").populate("friends");
+    res.json(users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// get a single user by their _id and thought/friend data
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id)
+      .populate("thoughts")
+      .populate("friends");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
-  });
+    res.json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
