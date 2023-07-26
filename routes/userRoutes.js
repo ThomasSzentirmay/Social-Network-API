3504e377cc6829a269f740a643a6d1ce60a43ba0
ThomasSzentirmay <132217664+ThomasSzentirmay@users.noreply.github.com>
2023-07-25 21:19:21 -0400

    delete to remove friend from users friend list

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -86,4 +86,22 @@ router.post("/:userId/friends/:friendId", async (req, res) => {
   }
 });
 
+// delete to remove a friend from a user's friend list
+router.delete("/:userId/friends/:friendId", async (req, res) => {
+  try {
+    const { userId, friendId } = req.params;
+    const user = await User.findByIdAndUpdate(
+      userId,
+      { $pull: { friends: friendId } },
+      { new: true }
+    );
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
