cdac5a7e543eccaafa3cd7257a5e228613939749
ThomasSzentirmay <132217664+ThomasSzentirmay@users.noreply.github.com>
2023-07-25 21:14:39 -0400

    wrote get all users route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,6 +3,14 @@ const { User } = require('../models');
 
 const router = express.Router();
 
-
+// get all the users
+router.get('/', async (req, res) => {
+    try {
+      const users = await User.find().populate('thoughts').populate('friends');
+      res.json(users);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 module.exports = router;
