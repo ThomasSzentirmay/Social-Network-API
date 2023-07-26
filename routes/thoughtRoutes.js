8f39788dca0fe13ebedfe734eb7cd0e30658bb61
ThomasSzentirmay <132217664+ThomasSzentirmay@users.noreply.github.com>
2023-07-25 21:23:41 -0400

    finished though routes

--- routes/thoughtRoutes.js
+++ routes/thoughtRoutes.js
@@ -55,3 +55,60 @@ router.put("/:id", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+// DELETE to remove a thought by its _id
+router.delete("/:id", async (req, res) => {
+  try {
+    const thought = await Thought.findByIdAndDelete(req.params.id);
+    if (!thought) {
+      return res.status(404).json({ message: "Thought not found" });
+    }
+    await User.findByIdAndUpdate(
+      thought.userId,
+      { $pull: { thoughts: thought._id } },
+      { new: true }
+    );
+    res.json(thought);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// POST to create a reaction for a thought
+router.post("/:thoughtId/reactions", async (req, res) => {
+  try {
+    const thought = await Thought.findById(req.params.thoughtId);
+    if (!thought) {
+      return res.status(404).json({ message: "Thought not found" });
+    }
+    const reaction = {
+      reactionBody: req.body.reactionBody,
+      username: req.body.username,
+    };
+    thought.reactions.push(reaction);
+    const updatedThought = await thought.save();
+    res.json(updatedThought);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// DELETE to remove a reaction from a thought
+router.delete("/:thoughtId/reactions/:reactionId", async (req, res) => {
+  try {
+    const { thoughtId, reactionId } = req.params;
+    const thought = await Thought.findByIdAndUpdate(
+      thoughtId,
+      { $pull: { reactions: { reactionId } } },
+      { new: true }
+    );
+    if (!thought) {
+      return res.status(404).json({ message: "Thought not found" });
+    }
+    res.json(thought);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
