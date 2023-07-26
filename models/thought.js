d2f5207d5584de9d08391af0dccc77f15710e524
ThomasSzentirmay <132217664+ThomasSzentirmay@users.noreply.github.com>
2023-07-25 23:16:25 -0400

    added reaction schema to thought model

--- models/thought.js
+++ models/thought.js
@@ -36,7 +36,7 @@ const thoughtSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  reactions: [reactionSchema],
+  reactions: [reactionSchema], 
 });
 
 thoughtSchema.virtual('reactionCount').get(function () {
