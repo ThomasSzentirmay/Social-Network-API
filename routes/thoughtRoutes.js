aec3d354fbc5a4b14eeac71d25c82c9a1ae298da
ThomasSzentirmay <132217664+ThomasSzentirmay@users.noreply.github.com>
2023-07-25 21:11:45 -0400

    created user model

---
+++ routes/thoughtRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const { User, Thought } = require('../models');
+
+const router = express.Router();
+
+
+
+module.exports = router;
---
+++ routes/userRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const { User } = require('../models');
+
+const router = express.Router();
+
+
+
+module.exports = router;
