3e8839238b14e35f0a2d9ebecb99701a2a90c429
Thomas Szentirmay <tlszent@outlook.com>
2023-07-27 20:23:13 -0400

    reconfigured thought model and deleted reaction model

--- models/thought.js
+++ models/thought.js
@@ -18,6 +18,31 @@ const thoughtSchema = new mongoose.Schema({
   reactions: [reactionSchema], 
 });
 
+const reactionSchema = new mongoose.Schema({
+  reactionId: {
+    type: mongoose.Schema.Types.ObjectId,
+    default: () => new mongoose.Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (createdAt) => new Date(createdAt).toISOString(),
+  },
+});
+
+thoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
+});
+
 const Thought = mongoose.model('Thought', thoughtSchema);
 
 module.exports = Thought;
